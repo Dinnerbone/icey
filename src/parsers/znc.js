6e9c9b35217c532d7b8489bb47af9f75455688d9
Nathan Adams <dinnerbone@dinnerbone.com>
2016-08-09 22:11:39 +0200

    Times should be moments

--- src/parsers/znc.js
+++ src/parsers/znc.js
@@ -1,6 +1,7 @@
 const XRegExp = require('xregexp');
 const fs = require('mz/fs');
 const es = require('event-stream');
+const moment = require('momentjs');
 
 class ZncParser {
     constructor(counter) {
@@ -12,46 +13,48 @@ class ZncParser {
             system: XRegExp.escape('***'),
         };
 
+        const toMoment = (date, time) => moment(`${date} ${time} Z`, 'YYYY-MM-DD HH:mm:ss Z');
+
         this.patterns = [
             {
                 pattern: XRegExp.build('^{{time}} \\* (?<nick>\\S+) (?<action>.*)$', subs),
-                result: (match, date) => this.counter.onAction(`${date} ${match.time}`, match.nick, match.action),
+                result: (match, date) => this.counter.onAction(toMoment(date, match.time), match.nick, match.action),
             },
             {
                 pattern: XRegExp.build('^{{time}} <(?<nick>[^>]+)> (?<message>.*)$', subs),
-                result: (match, date) => this.counter.onMessage(`${date} ${match.time}`, match.nick, match.message),
+                result: (match, date) => this.counter.onMessage(toMoment(date, match.time), match.nick, match.message),
             },
             {
                 pattern: XRegExp.build('^{{time}} {{system}} Joins: (?<nick>\\S+) {{hostmask}}$', subs),
-                result: (match, date) => this.counter.onJoin(`${date} ${match.time}`, match.nick),
+                result: (match, date) => this.counter.onJoin(toMoment(date, match.time), match.nick),
             },
             {
                 pattern: XRegExp.build('{{time}} {{system}} (?<victimNick>\\S+) was kicked by (?<nick>\\S+) \\((?<message>.+)\\)$', subs),
-                result: (match, date) => this.counter.onKick(`${date} ${match.time}`, match.nick, match.victimNick, match.message),
+                result: (match, date) => this.counter.onKick(toMoment(date, match.time), match.nick, match.victimNick, match.message),
             },
             {
                 pattern: XRegExp.build('^{{time}} {{system}} (?<nick>\\S+) sets mode: (?<modes>.+)$', subs),
-                result: (match, date) => this.counter.onMode(`${date} ${match.time}`, match.nick, match.modes),
+                result: (match, date) => this.counter.onMode(toMoment(date, match.time), match.nick, match.modes),
             },
             {
                 pattern: XRegExp.build('{{time}} {{system}} (?<nick>\\S+) is now known as (?<newNick>\\S+)$', subs),
-                result: (match, date) => this.counter.onNick(`${date} ${match.time}`, match.nick, match.newNick),
+                result: (match, date) => this.counter.onNick(toMoment(date, match.time), match.nick, match.newNick),
             },
             {
                 pattern: XRegExp.build('^{{time}} -(?<nick>[^-]+)- (?<message>.*)$', subs),
-                result: (match, date) => this.counter.onNotice(`${date} ${match.time}`, match.nick, match.message),
+                result: (match, date) => this.counter.onNotice(toMoment(date, match.time), match.nick, match.message),
             },
             {
                 pattern: XRegExp.build('^{{time}} {{system}} Parts: (?<nick>\\S+) {{hostmask}} \\((?<message>.*)\\)$', subs),
-                result: (match, date) => this.counter.onPart(`${date} ${match.time}`, match.nick, match.message),
+                result: (match, date) => this.counter.onPart(toMoment(date, match.time), match.nick, match.message),
             },
             {
                 pattern: XRegExp.build('^{{time}} {{system}} Quits: (?<nick>\\S+) {{hostmask}} \\((?<message>.*)\\)$', subs),
-                result: (match, date) => this.counter.onQuit(`${date} ${match.time}`, match.nick, match.message),
+                result: (match, date) => this.counter.onQuit(toMoment(date, match.time), match.nick, match.message),
             },
             {
                 pattern: XRegExp.build('^{{time}} {{system}} (?<nick>\\S+) changes topic to \'(?<topic>.*)\'$', subs),
-                result: (match, date) => this.counter.onTopic(`${date} ${match.time}`, match.nick, match.topic),
+                result: (match, date) => this.counter.onTopic(toMoment(date, match.time), match.nick, match.topic),
             },
         ];
     }
--- test/parsers/znc/index.js
+++ test/parsers/znc/index.js
@@ -4,6 +4,7 @@ const sinon = require('sinon');
 const expect = chai.expect;
 const ZncParser = require('../../../src/parsers/znc');
 const Counter = require('../../../src/counter');
+const moment = require('momentjs');
 
 describe('Parsing a line', () => {
     let counter;
@@ -18,13 +19,13 @@ describe('Parsing a line', () => {
         it('Ordinary message', () => {
             const onMessage = sinon.spy(counter, 'onMessage');
             parser.parseLine('2001-01-01', '[01:23:45] <Dinnerbone> Hello world! :)');
-            expect(onMessage).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Hello world! :)');
+            expect(onMessage).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Hello world! :)');
         });
 
         it('Empty message', () => {
             const onMessage = sinon.spy(counter, 'onMessage');
             parser.parseLine('2001-01-01', '[01:23:45] <Dinnerbone>  ');
-            expect(onMessage).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', ' ');
+            expect(onMessage).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', ' ');
         });
     });
 
@@ -32,33 +33,33 @@ describe('Parsing a line', () => {
         it('Ordinary action', () => {
             const onAction = sinon.spy(counter, 'onAction');
             parser.parseLine('2001-01-01', '[01:23:45] * Dinnerbone does an action');
-            expect(onAction).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'does an action');
+            expect(onAction).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'does an action');
         });
 
         it('Empty action', () => {
             const onAction = sinon.spy(counter, 'onAction');
             parser.parseLine('2001-01-01', '[01:23:45] * Dinnerbone ');
-            expect(onAction).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '');
+            expect(onAction).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '');
         });
     });
 
     it('Parsing a join', () => {
         const onJoin = sinon.spy(counter, 'onJoin');
         parser.parseLine('2001-01-01', '[01:23:45] *** Joins: Dinnerbone (dinnerbone@dinnerbone.com)');
-        expect(onJoin).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone');
+        expect(onJoin).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone');
     });
 
     describe('Parsing a kick', () => {
         it('Ordinary kick', () => {
             const onKick = sinon.spy(counter, 'onKick');
             parser.parseLine('2001-01-01', '[01:23:45] *** Troll was kicked by Dinnerbone (You suck!)');
-            expect(onKick).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Troll', 'You suck!');
+            expect(onKick).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Troll', 'You suck!');
         });
 
         it('Empty message', () => {
             const onKick = sinon.spy(counter, 'onKick');
             parser.parseLine('2001-01-01', '[01:23:45] *** Troll was kicked by Dinnerbone ( )');
-            expect(onKick).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Troll', ' ');
+            expect(onKick).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Troll', ' ');
         });
     });
 
@@ -66,45 +67,45 @@ describe('Parsing a line', () => {
         it('Adding a single mode', () => {
             const onMode = sinon.spy(counter, 'onMode');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone sets mode: +m');
-            expect(onMode).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '+m');
+            expect(onMode).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '+m');
         });
 
         it('Removing a single mode', () => {
             const onMode = sinon.spy(counter, 'onMode');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone sets mode: -m');
-            expect(onMode).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '-m');
+            expect(onMode).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '-m');
         });
 
         it('Adding a single mode with parameter', () => {
             const onMode = sinon.spy(counter, 'onMode');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone sets mode: +v voice');
-            expect(onMode).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '+v voice');
+            expect(onMode).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '+v voice');
         });
 
         it('Adding and removing multiple modes with parameters', () => {
             const onMode = sinon.spy(counter, 'onMode');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone sets mode: +vm-oS+k voice op key');
-            expect(onMode).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '+vm-oS+k voice op key');
+            expect(onMode).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '+vm-oS+k voice op key');
         });
     });
 
     it('Parsing a name change', () => {
         const onNick = sinon.spy(counter, 'onNick');
         parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone is now known as Djinnibone');
-        expect(onNick).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Djinnibone');
+        expect(onNick).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Djinnibone');
     });
 
     describe('Parsing a notice', () => {
         it('Ordinary notice', () => {
             const onNotice = sinon.spy(counter, 'onNotice');
             parser.parseLine('2001-01-01', '[01:23:45] -Dinnerbone- Hey, listen!');
-            expect(onNotice).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Hey, listen!');
+            expect(onNotice).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Hey, listen!');
         });
 
         it('Empty notice', () => {
             const onNotice = sinon.spy(counter, 'onNotice');
             parser.parseLine('2001-01-01', '[01:23:45] -Dinnerbone-  ');
-            expect(onNotice).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', ' ');
+            expect(onNotice).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', ' ');
         });
     });
 
@@ -112,13 +113,13 @@ describe('Parsing a line', () => {
         it('Ordinary part', () => {
             const onPart = sinon.spy(counter, 'onPart');
             parser.parseLine('2001-01-01', '[01:23:45] *** Parts: Dinnerbone (dinnerbone@dinnerbone.com) (Goodbye, cruel channel!)');
-            expect(onPart).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Goodbye, cruel channel!');
+            expect(onPart).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Goodbye, cruel channel!');
         });
 
         it('Empty message', () => {
             const onPart = sinon.spy(counter, 'onPart');
             parser.parseLine('2001-01-01', '[01:23:45] *** Parts: Dinnerbone (dinnerbone@dinnerbone.com) ()');
-            expect(onPart).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '');
+            expect(onPart).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '');
         });
     });
 
@@ -126,13 +127,13 @@ describe('Parsing a line', () => {
         it('Ordinary part', () => {
             const onQuit = sinon.spy(counter, 'onQuit');
             parser.parseLine('2001-01-01', '[01:23:45] *** Quits: Dinnerbone (dinnerbone@dinnerbone.com) (Goodbye, cruel network!)');
-            expect(onQuit).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'Goodbye, cruel network!');
+            expect(onQuit).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'Goodbye, cruel network!');
         });
 
         it('Empty message', () => {
             const onQuit = sinon.spy(counter, 'onQuit');
             parser.parseLine('2001-01-01', '[01:23:45] *** Quits: Dinnerbone (dinnerbone@dinnerbone.com) ()');
-            expect(onQuit).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '');
+            expect(onQuit).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '');
         });
     });
 
@@ -140,13 +141,13 @@ describe('Parsing a line', () => {
         it('Ordinary topic', () => {
             const onTopic = sinon.spy(counter, 'onTopic');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone changes topic to \'It\'s a channel about nothing!\'');
-            expect(onTopic).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', 'It\'s a channel about nothing!');
+            expect(onTopic).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', 'It\'s a channel about nothing!');
         });
 
         it('Empty message', () => {
             const onTopic = sinon.spy(counter, 'onTopic');
             parser.parseLine('2001-01-01', '[01:23:45] *** Dinnerbone changes topic to \'\'');
-            expect(onTopic).to.have.been.calledOnce.calledWithExactly('2001-01-01 01:23:45', 'Dinnerbone', '');
+            expect(onTopic).to.have.been.calledOnce.calledWithExactly(moment('2001-01-01T01:23:45Z'), 'Dinnerbone', '');
         });
     });
 });
